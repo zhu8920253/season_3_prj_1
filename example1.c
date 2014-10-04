98d79a9f9ac36c9f368ee2b6c6d3688cf5b37860
M_O_Bz <M_O_Bz@163.com>
2014-10-05 01:53:04 +0800

    添加打印信息

--- example1.c
+++ example1.c
@@ -11,6 +11,7 @@
 
 #include <ft2build.h>
 #include FT_FREETYPE_H
+#include FT_GLYPH_H
 
 
 #define WIDTH   80
@@ -32,6 +33,8 @@ draw_bitmap( FT_Bitmap*  bitmap,
   FT_Int  x_max = x + bitmap->width;
   FT_Int  y_max = y + bitmap->rows;
 
+
+	//printf("x = %d, y = %d\n", x, y);
 
   for ( i = x, p = 0; i < x_max; i++, p++ )
   {
@@ -55,6 +58,7 @@ show_image( void )
 
   for ( i = 0; i < HEIGHT; i++ )
   {
+  	printf("%02d", i);
     for ( j = 0; j < WIDTH; j++ )
       putchar( image[i][j] == 0 ? ' '
                                 : image[i][j] < 128 ? '+'
@@ -85,10 +89,11 @@ main( int     argc,
 
   //int chinese_str[] = {0x97e6, 0x4e1c, 0x5c71, 0x0067};
   //char *str = "韦东山";
-//  wchar_t *chinese_str = L"韦东山g";
-  wchar_t *chinese_str = L"萨芬";
-  unsigned int *p = (unsigned int *)chinese_str;
+  wchar_t *chinese_str = L"韦gif";
+  unsigned int *p = (wchar_t *)chinese_str;
   int i;
+  FT_BBox bbox;
+  FT_Glyph  glyph;
 
 	printf("Uniocde: \n");
   for (i = 0; i < wcslen(chinese_str); i++)
@@ -150,10 +155,24 @@ main( int     argc,
     if ( error )
       continue;                 /* ignore errors */
 
+    error = FT_Get_Glyph( face->glyph, &glyph );
+	if (error)
+	{
+		printf("FT_Get_Glyph error!\n");
+		return -1;
+	}
+
+    FT_Glyph_Get_CBox(glyph, FT_GLYPH_BBOX_TRUNCATE, &bbox );
+
     /* now, draw to our target surface (convert position) */
     draw_bitmap( &slot->bitmap,
                  slot->bitmap_left,
                  target_height - slot->bitmap_top );
+
+	printf("Unicode: 0x%x\n", chinese_str[n]);
+	printf("origin.x/64 = %d, origin.y/64 = %d\n", pen.x/64, pen.y/64);
+	printf("xMin = %d, xMax = %d, yMin = %d, yMax = %d\n", bbox.xMin, bbox.xMax, bbox.yMin, bbox.yMax);
+    printf("slot->advance.x/64 = %d, slot->advance.y/64 = %d\n", slot->advance.x/64, slot->advance.y/64);
 
     /* increment pen position */
     pen.x += slot->advance.x;
